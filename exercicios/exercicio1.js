b2cb5f44ab9df79a330093d7c40087bb959c9b1e
PedroLucas <pedrolucasamotta0@gmail.com>
2025-12-29 11:28:39 -0300

    Adiciona validação se é equação de segundo grau

--- exercicios/exercicio1.js
+++ exercicios/exercicio1.js
@@ -2,20 +2,26 @@ const a = Number(prompt("Digite o valor de A:"));
 const b = Number(prompt("Digite o valor de B:"));
 const c = Number(prompt("Digite o valor de C:"));
 
-const delta = b * b - 4 * a * c;
+if (a === 0) {
+    console.log("Isso não é uma equação do segundo grau.");
+    return;
+} else {
 
-console.log("O valor de delta é: " + delta);
-
-if (delta > 0) {
+  const delta = b * b - 4 * a * c;
+  
+  console.log("O valor de delta é: " + delta);
+  
+  if (delta > 0) {
     const x1 = (-b + Math.sqrt(delta)) / (2 * a);
     const x2 = (-b - Math.sqrt(delta)) / (2 * a);
     console.log("Duas raízes reais:");
     console.log("x1 = " + x1);
     console.log("x2 = " + x2);
-} else if (delta === 0) {
+  } else if (delta === 0) {
     const x = -b / (2 * a);
     console.log("Uma raiz real:");
     console.log("x = " + x);
-} else {
+  } else {
     console.log("Não há raízes reais.");
+  }
 }

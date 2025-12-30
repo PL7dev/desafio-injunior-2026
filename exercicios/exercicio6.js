cf2b679c09fe172ab86d50fc86186bf167855e21
PedroLucas <pedrolucasamotta0@gmail.com>
2025-12-30 11:21:40 -0300

    Resolução do exercício 6

---
+++ exercicios/exercicio6.js
@@ -0,0 +1,21 @@
+function fibonacci (n) {
+    if (n ===0) return 0;
+    if (n ===1) return 1;
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+const quantidade = Number(prompt("Digite o número de termos da sequência:"))
+
+if (!Number.isInteger(quantidade) || quantidade <= 0) {
+    console.log("Digite um número inteiro positivo válido");
+} else {
+    const termos = [];
+    
+    for (let i = 0; i < quantidade; i++) {
+        termos.push(fibonacci(i));
+    }
+}
+
+console.log("Sequência de Fibonacci:");
+console.log(termos.join(", "));
